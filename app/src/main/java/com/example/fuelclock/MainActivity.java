7e56b440555b8482ed8e3641a05bfd89d4f85539
moto_3 <irie.rs.motosuke@gmail.com>
2019-09-10 18:53:11 +0900

    Added a daemon thread to increase dEstFuel every second for testing.

--- app/src/main/java/com/example/fuelclock/MainActivity.java
+++ app/src/main/java/com/example/fuelclock/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.icu.text.SimpleDateFormat;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -17,33 +18,47 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ハンドラを生成
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            // 予定燃料消費率を計算
+            double dEstLPM = 0;
+            TextView indicaterEstFuel = findViewById(R.id.tvEstFuel);
+            @Override
+            public void run() {
+                indicaterEstFuel.setText(String.format("%1$.2f", dEstLPM));
+                dEstLPM+=0.1;
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(runnable);
 
         // 時刻表示フォーマットを設定
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 
-        // 予定燃料消費率を計算
-        double decEstLPM = 0.3;
+        /* 予定燃料消費率を計算
+        double dEstLPM = 0.3;*/
 
         // 現在時刻を取得
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+9") );
         Date dateTimeNow = new Date();
         dateTimeNow.setTime(cal.getTimeInMillis());
-        // 予定燃料消費を計算
 
         // 現在時刻を表示
         TextView indicaterTimeNow = findViewById(R.id.tvTimeNow);
         indicaterTimeNow.setText(sdf.format(dateTimeNow));
 
-        // 予定燃料消費を表示
+        /* 予定燃料消費を表示
         TextView indicaterEstFuel = findViewById(R.id.tvEstFuel);
-        indicaterEstFuel.setText(String.format("%1$.2f", decEstLPM));
+        indicaterEstFuel.setText(String.format("%1$.2f", dEstLPM));*/
 
-        // スタート時刻入力
+        // スタート時刻をクリックすると入力モードに入る
         EditText inputStartTime = findViewById(R.id.etStartTime);
         inputStartTime.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // フィニッシュ時刻入力
+        // フィニッシュ時刻をクリックすると入力モードに入る
         EditText inputFinishTime = findViewById(R.id.etFinishTime);
         inputFinishTime.setOnClickListener(new View.OnClickListener() {
             @Override
