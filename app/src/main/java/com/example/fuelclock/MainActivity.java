3cad363195f949b8cd7df14f1ab4c6624fa824a9
moto_3 <irie.rs.motosuke@gmail.com>
2019-09-11 18:50:42 +0900

    Trying to add EstFuel calculation.

--- app/src/main/java/com/example/fuelclock/MainActivity.java
+++ app/src/main/java/com/example/fuelclock/MainActivity.java
@@ -3,14 +3,10 @@ package com.example.fuelclock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.icu.text.SimpleDateFormat;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextClock;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -24,39 +20,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ハンドラを生成
+        // 1秒ごとに予定燃料消費量を再計算・表示するデーモンスレッド
         final Handler handler = new Handler();
         final Runnable runnable = new Runnable() {
-            // 予定燃料消費率を計算
             double dEstLPM = 0;
+            double dFullTank;
             TextView indicaterEstFuel = findViewById(R.id.tvEstFuel);
+            EditText inputStartTime = findViewById(R.id.etStartTime);
+            EditText inputFinishTime = findViewById(R.id.etFinishTime);
+            EditText inputFullTank = findViewById(R.id.etFullTank);
+            Date dateTimeNow = new Date();
+            SimpleDateFormat sdf_HHmm = new SimpleDateFormat("HH:mm");
+            SimpleDateFormat sdf_HHmmss = new SimpleDateFormat("HH:mm:ss");
+            Date dateSatrtTime = new Date();
+            Date dateFinishTime = new Date();
+
             @Override
             public void run() {
-                indicaterEstFuel.setText(String.format("%1$.2f", dEstLPM));
+                // スタート・フィニッシュ時刻を取得
+                String sStartTime = inputStartTime.getText().toString();
+                String sFinishTime = inputFinishTime.getText().toString();
+                try {
+                    dateSatrtTime = sdf_HHmm.parse(sStartTime);
+                    dateFinishTime = sdf_HHmm.parse(sFinishTime);
+                } catch (Exception e) {
+                    dateSatrtTime.setTime(0);
+                    dateFinishTime.setTime(0);
+                }
+                String sFullTank = inputFullTank.getText().toString();
+                dFullTank = Double.parseDouble(sFullTank);
+
+                // 現在時刻を取得
+                Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tokyo"));
+                dateTimeNow.setTime(cal.get(Calendar.HOUR) * 3600000 + cal.get(Calendar.MINUTE) * 60000 + cal.get(Calendar.SECOND) * 1000);
+
+                // 現在時刻を表示
+                TextView indicaterTimeNow = findViewById(R.id.tvTimeNow);
+                indicaterTimeNow.setText(sdf_HHmmss.format(dateTimeNow));
+
+                // 予定燃料消費量を再計算・表示
                 dEstLPM+=0.1;
+                indicaterEstFuel.setText(String.format("%1$.2f", dEstLPM));
                 handler.postDelayed(this, 1000);
             }
         };
         handler.post(runnable);
-
-        // 時刻表示フォーマットを設定
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-
-        /* 予定燃料消費率を計算
-        double dEstLPM = 0.3;*/
-
-        // 現在時刻を取得
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+9") );
-        Date dateTimeNow = new Date();
-        dateTimeNow.setTime(cal.getTimeInMillis());
-
-        // 現在時刻を表示
-        TextView indicaterTimeNow = findViewById(R.id.tvTimeNow);
-        indicaterTimeNow.setText(sdf.format(dateTimeNow));
-
-        /* 予定燃料消費を表示
-        TextView indicaterEstFuel = findViewById(R.id.tvEstFuel);
-        indicaterEstFuel.setText(String.format("%1$.2f", dEstLPM));*/
 
         // スタート時刻をクリックすると入力モードに入る
         EditText inputStartTime = findViewById(R.id.etStartTime);
